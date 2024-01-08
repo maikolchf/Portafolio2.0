448e165433d3f6603f4ad4b70a609346b356982f
Michael Chavarria Flores <maikolchf@outlook.com>
2024-01-07 21:02:07 -0600

    Se agrega parte de mis estudios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerComponent } from './banner/banner.component';
 import { AboutComponent } from './about/about.component';
+import { TracksComponent } from './tracks/tracks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
-    AboutComponent
+    AboutComponent,
+    TracksComponent
   ],
   imports: [
     BrowserModule,
